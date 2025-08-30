b6fbcf56167b153766eb4294d8b5c05ab1da96f4
Wilberto Morales <wilbertomorales777@gmail.com>
2025-08-29 22:27:48 -0500

    Sample fastify server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import Fastify from "fastify";
+
+const server = new Fastify({
+  logger: true,
+});
+
+server.get("/", (request, reply) => {
+  reply.send({ hello: "world" });
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+});
