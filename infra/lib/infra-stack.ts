ef651bbdaefcb6035d00885cc8e2b74fc7126c9a
Wilberto Morales <wilbertomorales777@gmail.com>
2025-08-30 21:32:12 -0500

    Stub infrastructure for test messages

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as eventsources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
+
+const codeLines = [
+  `const util = require('util');`,
+  `function handler(event, ctx, cb) {`,
+  `  console.log({event: util.inspect(event, {depth: Infinity})});`,
+  `  return cb(null, "hi");`,
+  `}`,
+  `exports.handler = handler;`,
+];
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    {
+      const topic = new sns.Topic(this, "topic", {});
+      const queue = new sqs.Queue(this, "queue", {});
+      const fn = new lambda.Function(this, "lambda", {
+        runtime: lambda.Runtime.NODEJS_LATEST,
+        code: lambda.Code.fromInline(codeLines.join("\n")),
+        handler: `index.handler`,
+      });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // SNS -> SQS -> Lambda
+      topic.addSubscription(new subscriptions.SqsSubscription(queue));
+      fn.addEventSource(new eventsources.SqsEventSource(queue));
+
+      new cdk.CfnOutput(this, "TopicARN", {
+        value: topic.topicArn,
+      });
+
+      new cdk.CfnOutput(this, "FunctionName", {
+        value: fn.functionName,
+      });
+    }
   }
 }
